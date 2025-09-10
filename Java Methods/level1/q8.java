8236a103124c4974af0ab6f357799252064b3e80
Nandani-Maheshwari <nandumahesh101@gmail.com>
2025-09-10 23:12:33 +0530

    Add files via upload

---
+++ Java Methods/level1/q8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class q8{
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[] {quotient, remainder};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+        if (divisor == 0) {
+            System.out.println("Error: Division by zero is not allowed!");
+        } else {
+            int[] result = findRemainderAndQuotient(number, divisor);
+            System.out.println("Quotient = " + result[0]);
+            System.out.println("Remainder = " + result[1]);
+        }
+        sc.close();
+    }
+}
+
